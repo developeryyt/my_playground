b14a02309e8526ac08b3307eb632cd5ca7ead849
developeryyt <yeontaey92@gmail.com>
2025-03-14 17:28:08 +0900

    로그인페이지 작업중

---
+++ src/app/login/page.tsx
@@ -0,0 +1,27 @@
+import Lock from "@/components/icons/Lock";
+import LoginForm from "@/components/LoginForm";
+
+const Page = () => {
+    return (
+        <div className='h-full'>
+            <div className='h-full flex'>
+                <div className='flex-1 flex items-center bg-brandcolor2'>
+                    <dl className='flex flex-1 flex-col items-center'>
+                        <dt className='text-center'>
+                            <span className='block'>
+                                <Lock/>
+                            </span>
+                            <strong className='font-semibold text-5xl'>Welcome Back!</strong>
+                        </dt>
+                        <dd>For Authorized Person Only</dd>
+                    </dl>
+                </div>
+                <div className='flex-1'>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,14 @@
+import Login from "@/components/Login";
+
+const LoginForm = () => {
+    return (
+        <div>
+            <section>
+                <h3>Login</h3>
+                <Login />
+            </section>
+        </div>
+    );
+};
+
+export default LoginForm;
